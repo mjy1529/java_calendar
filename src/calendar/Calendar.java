d2f37d997deba73593d642f3126de26618f644a3
문주영 <enqlenwndud95@gmail.com>
2017-08-21 21:26:45 +0900

    Add repeat function

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,8 +3,13 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	private static final int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public static int getMaxDayOfMonth(int num) {
+		return maxDays[num - 1];
+	}
+	
 	public static void main(String[] args) {
-		
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
@@ -13,12 +18,15 @@ public class Calendar {
 		System.out.println("22 23 24 25 26 27 28");
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		System.out.println("\n원하는 달의 숫자를 입력해주세요 : ");
+		System.out.println("\n반복 횟수를 입력하세요. : ");
 		Scanner scanner = new Scanner(System. in);
-		int num = scanner.nextInt();
-		
-		int maxDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.printf("%d월은 %d일까지 있습니다.\n", num, maxDays[num-1]);
+		int repeat = scanner.nextInt();
+		System.out.println("달을 입력하세요.");
+		for(int i=0; i < repeat; i++) {
+			int month = scanner.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, getMaxDayOfMonth(month));
+		}
+		System.out.println("안녕~");
 		scanner.close();
 	}
 }
